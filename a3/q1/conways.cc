6bda97ad8343085a0cb2d254488c520a72e727e7
Victor Kwok <tinhangv@ualberta.ca>
2024-03-22 13:49:45 -0600

    13:49

--- a3/q1/conways.cc
+++ a3/q1/conways.cc
@@ -28,22 +28,24 @@ int main(){
     //s: progress grid by one time step
     //p: print grid
     char command;
-    string tempGrid;
+    string tempGrid="";
     while(cin>>command){
         if(command == 's'){
+            tempGrid = "";
             for(size_t row=0; row<rows; ++row) 
             for(size_t col=0;col<columns;++col){
                 int neighbors =0;
                 int checks[8][2] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
                 //do checks
                 for(size_t t=0; t<8; ++t){
-                    int i=row+checks[i][0];
-                    int j=col+checks[i][1];
+                    int i=row+checks[t][0];
+                    int j=col+checks[t][1];
                     if(i>=0 && i<rows && j>=0 && j<columns){
                         char neighbor = grid[columns*i+j];
                         if(neighbor == '0') ++neighbors;
                     }
                 }
+                
                 if(neighbors==3) tempGrid += '0';
                 else tempGrid += '.';
             }
