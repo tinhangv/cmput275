7c432edf09e9ae816ca230b0ccf21a7358e7735e
Victor Kwok <tinhangv@ualberta.ca>
2024-03-23 18:48:14 -0600

    q2 solved

--- a3/q2/2DMotion.cc
+++ a3/q2/2DMotion.cc
@@ -2,8 +2,44 @@
 #include <iomanip>
 #include <string>
 #include <sstream>
+#include <cmath>
+#include "2DMotion.h"
+using namespace std;
 
-int main(){
+//point constructor
+Point::Point(double x, double y): x{x},y{y} {}
+Point::Point():x{0},y{0}{}
 
-    
+int Point::quadrant(){
+    return y>=0 ? (x>=0 ? 1: 2) : (x<0 ? 3: 4);
+}
+
+Point operator+(Point p, Force F){
+    return Point{p.x+F.mag*cos(F.angle*PI/180), p.y+F.mag*sin(F.angle*PI/180)};
+}
+
+//point input output operators
+istream &operator>>(istream &in, Point &p){
+    return in >> p.x >> p.y;
+}
+ostream &operator<<(ostream &out, const Point &p){
+    out << fixed << setprecision(2);
+    return out << "(" << p.x << ", " << p.y << ")";
+}
+
+//force constructor
+Force::Force(double angle, double mag): angle{angle},mag{mag} {}
+Force::Force():angle{0},mag{0}{}
+
+//force input output operators
+istream &operator>>(istream &in, Force &F){
+    return in >> F.angle >> F.mag;
+}
+ostream &operator<<(ostream &out, const Force &F){
+    out << fixed << setprecision(2);
+    return out << F.angle << " degrees with magnitude of " << F.mag;
+}
+
+Force operator*(Force F, int i){
+    return Force{F.angle, F.mag*i};
 }
--- a3/q2/2DMotion.h
+++ a3/q2/2DMotion.h
@@ -1,6 +1,11 @@
 #ifndef MOTION2D
 #define MOTION2D
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+using namespace std;
 
 // Predefined constant for pi, to be used if/when converting degrees
 // to radians.
@@ -12,15 +17,45 @@ class Force;
 
 class Point {
     // Private fields and helpers here.
+    double x,y;
+    
   public:
     // Required public interface here.
 
+    //default constructor
+    Point(double x,double y);
+    Point();
+    //overloaded input operator
+    friend istream &operator>>(istream &, Point &);
+    //overloaded output operator
+    friend ostream &operator<<(ostream &, const Point &);
+    //quadrant function
+    int quadrant();
+    //overloaded addition operator btw point and force
+    friend Point operator+(Point, Force);
 };
+
 
 class Force {
     // Private fields and helpers here.
+    double angle, mag;
   public:
     // Required public interface here.
+
+    //default constructor
+    Force(double angle,double mag);
+    Force();
+    //overloaded input operator
+    friend istream &operator>>(istream &, Force &);
+    //overloaded output operator
+    friend ostream &operator<<(ostream &, const Force &);
+    //overloaded multiplication operator between Force and int
+    friend Force operator*(Force, int);
+    //overloaded addition operator btw point and force
+    friend Point operator+(Point, Force);
 };
 
+
+//overloaded addition operator btw point and force
+Point operator+(Point p, Force F);
 #endif
